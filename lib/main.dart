d2fd9ba5d4688d94bbaaeee26ec79a8aa08f4d75
p-golba <hvxenons@gmail.com>
2022-10-24 19:31:12 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import './transaction.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Personal Expenses App', home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key});
+
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'Hamburger', amount: 23.99, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Groceries', amount: 8.99, date: DateTime.now()),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Personal Expenses'),
+      ),
+      body: Column(children: [
+        const SizedBox(
+          width: double.infinity,
+          child: Card(
+            color: Colors.blueGrey,
+            child: Text('Card Placeholder'),
+          ),
+        ),
+        Column(
+          children: transactions.map((tx) {
+            return Card(
+              child: Row(children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    '\$${tx.amount}',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tx.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      DateFormat.yMMMEd().format(tx.date),
+                      style: const TextStyle(
+                        fontSize: 11,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                )
+              ]),
+            );
+          }).toList(),
+        )
+      ]),
+    );
+  }
+}
